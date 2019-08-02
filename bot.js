3f1c03a7746c2f9668954c127c858cefc9b64ca8
Zerplin <43621940+Zerplin@users.noreply.github.com>
2019-08-02 22:28:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 var rdy = 1;
 var mockList = [];
 require('dotenv').config();
+
+/*
+bot.on('message', (message)=> 
+{
+    if(message.content == 'noot'||message.content == 'Noot'||message.content == 'NOOT')
+        message.channel.sendMessage("**NOOT**")
+});
+*/
 
 function mockingSpongebob(text) 
 {
@@ -18,6 +27,17 @@ function mockingSpongebob(text)
     }
     return res;
 }
+/*
+channel.fetchMessages({ limit: 1 }).then(messages => {
+    let lastMessage = messages.first();
+  
+    if (!lastMessage.author.bot) {
+      // The author of the last message wasn't a bot
+      message.channel.sendMessage(mockingSpongebob(message.content));
+    }
+  })
+
+*/
 
 bot.on('message', (message)=>
 {
@@ -29,6 +49,8 @@ bot.on('message', (message)=>
         {
             console.log("before removal "+mockList);
             mockList = mockList.filter(user => user!=message.mentions.users.first().id);
+            //mockList.pop();
+            //mockList. mockList.indexOf(message.mentions.users.first().id)
             console.log("to be removed "+message.mentions.users.first().id);
             console.log("removed "+mockList);
         }
@@ -53,5 +75,8 @@ bot.on('message', (message)=>
 bot.login(process.env.token);
 
 console.log("check");
+//console.log(process.env.token);
+
+
 
 
