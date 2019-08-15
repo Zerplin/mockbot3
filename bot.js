d797a09a97b980da53267e12e05347fd5ff6b978
Zerplin <43621940+Zerplin@users.noreply.github.com>
2019-08-15 22:50:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,14 @@ var skip = 0;
 const prefix = "!";
 require('dotenv').config();
 
-//const DBL = require("dblapi.js");
-//const dbl = new DBL('process.env.token', bot);
-/*
+const DBL = require("dblapi.js");
+const dbl = new DBL('process.env.token', bot);
+
 bot.on("ready", () => {
   console.log("Ready");
   bot.user.setActivity('!cmd', { type: 'LISTENING' });
 })
-*/
+
 
 function mockingSpongebob(text) {
   var res = "";
